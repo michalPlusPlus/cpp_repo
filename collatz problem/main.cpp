c38418e1d7d2d42626aa1a1b38bb076073c88468
michalPlusPlus <michal.mistat@wp.pl>
2019-11-17 13:11:20 +0100

    gotowy program obrazujacy problem collatza

---
+++ collatz problem/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a(0), steps(0);
+
+    do {
+        cout << "Enter number (above 1): ";
+        cin.clear();
+        cin.sync();
+        cout << "\n";
+        cin >> a;
+    }
+    while (cin.fail() || a<2);
+
+
+    while (a!=1) {
+        if (a%2==1) {
+            steps = steps+1;
+            a = 3*a+1;
+            cout << a << endl;
+        }
+        else {
+            steps = steps+1;
+            a = a/2;
+            cout << a << endl;
+        }
+    }
+
+    cout << "\n";
+    cout << "Steps made: " << steps;
+
+    return 0;
+}
