f22472b8f33449438a96a8aaed22b5b82dfda682
michalPlusPlus <michal.mistat@wp.pl>
2019-11-21 19:48:21 +0100

    obsluga bledow, dodano mozliwosc wpisywania znakow; v2.0

--- tablice_spoj/main.cpp
+++ tablice_spoj/main.cpp
@@ -5,25 +5,39 @@
 
 using namespace std;
 
-int expm_amount(1), num_in_emp, temp;
-vector <int> numbers;
+int exmp_amount(1), num_amount(2); //num_cache(0);
+string locl_cache;
+vector <string> vec_numbers;
+
+int get_number(const char* text) {
+    int gn(1);
+    do {
+        cout << text;
+        cin.clear();
+        cin.sync();
+        cin >> gn;
+    }
+    while(cin.fail() || gn<1 || gn>10);
+    return gn;
+}
 
 int main() {
-    cout << "Enter examples amount: ";
-    cin >> expm_amount;
+    exmp_amount = get_number("Enter examples amount (max 10): ");
 
-    for (int i=0; i<expm_amount; i++) {
-        cout << "Numbers in example " << i+1 << ": ";
-        cin >> num_in_emp;
+    for (int i=0; i<exmp_amount; i++) {
+        cout << endl << "Example " << i+1;
+        num_amount = get_number(" amount of elements (max 10): ");
 
-        for (int j=0; j<num_in_emp; j++) {
-            cin >> temp;
-            numbers.push_back(temp);
+        for (int j=0; j<num_amount; j++) {
+            cin >> locl_cache;
+            vec_numbers.push_back(locl_cache);
         }
-        for (int k=numbers.size()-1; k<=numbers.size(); k--) {
-            cout << numbers[k];
+
+        for (int k=vec_numbers.size()-1; k<=vec_numbers.size(); k--) {
+            cout << vec_numbers[k] << " ";
         }
-        numbers.clear();
+
+        vec_numbers.clear();
         cout << endl;
     }
 
